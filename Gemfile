7e150cfd81f92957dfc72352d73a15bdcd3b6d73
Fukuo Kadota <app.fk91@gmail.com>
2015-04-14 02:05:48 +0900

    Complete twitter#search

--- Gemfile
+++ Gemfile
@@ -2,7 +2,10 @@ source 'https://rubygems.org'
 
 ruby '2.1.1'
 
+gem 'dotenv'
 gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'slim'
+gem 'json'
 
+gem 'twitter'
--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'slim'
+require 'twitter'
+require 'dotenv'
+require 'json'
+Dotenv.load
 
-set :slim, format: :html5
+set :slim, format: :html
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key    = ENV['TODAYS_MONCHAN_CONSUMER_KEY']
+  config.consumer_secret = ENV['TODAYS_MONCHAN_CONSUMER_SECRET']
+end
 
 get '/' do
+  @result = client.search("#今日のもんちゃん -rt", lang: "ja", result_type: :recent, count: 100)
   slim :index
 end
